6f1a8eedd04eeaef16581d667501c93eb1eb1148
Glen Buck and Ross Fletcher <glen@>
2017-11-28 15:49:52 -0800

    add code to scripts.js to delete messsages

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,10 @@ $(document).ready(function() {
     $("ul#user").prepend("<li>Hello!</li>");
     $("ul#webpage").prepend("<li>Why hello there!</li>");
     $("ul#user").children("li").first().click(function() {
-        alert('hi');
+        $(this).remove();
       });
     $("ul#webpage").children("li").first().click(function() {
-        alert('hi');
+        $(this).remove();
       });
     });
 
@@ -14,10 +14,22 @@ $(document).ready(function() {
   $("button#goodbye").click(function() {
     $("ul#user").prepend("<li>Goodbye!</li>");
     $("ul#webpage").prepend("<li>Goodbye, dear user!</li>");
+    $("ul#user").children("li").first().click(function() {
+        $(this).remove();
+      });
+    $("ul#webpage").children("li").first().click(function() {
+        $(this).remove();
+      });
   });
 
   $("button#stop").click(function() {
     $("ul#user").prepend("<li>Stop copying me!</li>");
     $("ul#webpage").prepend("<li>Pardon me. I meant no offense.</li>");
+    $("ul#user").children("li").first().click(function() {
+        $(this).remove();
+      });
+    $("ul#webpage").children("li").first().click(function() {
+        $(this).remove();
+      });
   });
 });
